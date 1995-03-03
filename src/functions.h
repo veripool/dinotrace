8377872fc72963a0162c4510e993e990a9549153
Wilson Snyder <wsnyder@wsnyder.org>
1995-03-03 22:13:01 +0000

    Added inline functions.
    git-svn-id: file://localhost/svn/dinotrace/trunk/dinotrace@44 134a06a7-2fee-0310-9998-f5402a1fb3eb

--- src/functions.h
+++ src/functions.h
@@ -19,6 +19,46 @@
  *
  */
 
+/***********************************************************************/
+/* Defined functions.... Really inlines, but old VMS compiler chokes */
+
+#define	cptr_to_value(cptr,value_ptr) \
+{\
+     (value_ptr)->siglw.number = (cptr)[0].number;\
+     (value_ptr)->number[0] = (cptr)[1].number;\
+     (value_ptr)->number[1] = (cptr)[2].number;\
+     (value_ptr)->number[2] = (cptr)[3].number;\
+ }
+
+/* Also identical commented function in dt_file if this isn't defined */
+#define	fil_add_cptr(sig_ptr, value_ptr, check) \
+{\
+    SIGNAL_LW	*cptr;\
+    long diff = ((SIGNAL *)(sig_ptr))->cptr - ((SIGNAL *)(sig_ptr))->bptr;\
+    if (diff > BLK_SIZE / 4 * ((SIGNAL *)(sig_ptr))->blocks - 4) {\
+	((SIGNAL *)(sig_ptr))->blocks++;\
+	((SIGNAL *)(sig_ptr))->bptr = (SIGNAL_LW *)XtRealloc ((char*)((SIGNAL *)(sig_ptr))->bptr, ((SIGNAL *)(sig_ptr))->blocks*BLK_SIZE);\
+	((SIGNAL *)(sig_ptr))->cptr = ((SIGNAL *)(sig_ptr))->bptr+diff;\
+	}\
+    cptr = ((SIGNAL *)(sig_ptr))->cptr - ((SIGNAL *)(sig_ptr))->lws;\
+    if ( !(check)\
+	|| ( cptr->sttime.state != ((VALUE *)(value_ptr))->siglw.sttime.state )\
+	|| ( (cptr+1)->number != ((VALUE *)(value_ptr))->number[0] )\
+	|| ( (cptr+2)->number != ((VALUE *)(value_ptr))->number[1] )\
+	|| ( (cptr+3)->number != ((VALUE *)(value_ptr))->number[2] ) )\
+	{\
+	(((SIGNAL *)(sig_ptr))->cptr)[0].number = ((VALUE *)(value_ptr))->siglw.number;\
+	(((SIGNAL *)(sig_ptr))->cptr)[1].number = ((VALUE *)(value_ptr))->number[0];\
+	(((SIGNAL *)(sig_ptr))->cptr)[2].number = ((VALUE *)(value_ptr))->number[1];\
+	(((SIGNAL *)(sig_ptr))->cptr)[3].number = ((VALUE *)(value_ptr))->number[2];\
+	(((SIGNAL *)(sig_ptr))->cptr) += ((SIGNAL *)(sig_ptr))->lws;\
+	}\
+    }
+/*#undef	fil_add_cptr*/
+
+
+/***********************************************************************/
+
 /* Dinotrace.c */
 extern char	*help_message();
 
@@ -94,9 +134,11 @@ extern void
     free_data(),
     read_make_busses(),
     fil_string_to_value(),
-    fil_add_cptr(),
     decsim_read_ascii(), 
     read_trace_end(), help_cb(), help_trace_cb(), update_signal_states();
+#ifndef fil_add_cptr
+extern void fil_add_cptr();
+#endif
 
 /* dt_util routines */
 extern void
