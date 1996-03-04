f559ff8ec027a7929221d10c93a94ba0288fdc13
Wilson Snyder <wsnyder@wsnyder.org>
1996-03-04 22:01:16 +0000

    grid now calced from LAST assertions
    git-svn-id: file://localhost/svn/dinotrace/trunk/dinotrace@162 134a06a7-2fee-0310-9998-f5402a1fb3eb

--- src/dt_grid.c
+++ src/dt_grid.c
@@ -79,6 +79,8 @@ void	grid_calc_auto (trace, grid_ptr)
     cptr = sig_ptr->cptr;
     /* Skip to end */
     while ( cptr->sttime.time != EOT) cptr += sig_ptr->lws;
+    /* Ignore last - determined by EOT, not period */
+    while ( cptr != sig_ptr->cptr) cptr -= sig_ptr->lws;
 
     /* Move back 3 grids, if we can */
     while ( cptr != sig_ptr->cptr) {
